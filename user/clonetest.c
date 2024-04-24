0d29cf986dac91695c7d26fca40131b0613a286e
adriannic <spamadriannic@gmail.com>
2024-04-24 12:26:11 +0200

    fix(clonetest): fix test

--- user/clonetest.c
+++ user/clonetest.c
@@ -3,7 +3,6 @@
 #include "../user/user.h"
 
 int thread_fn(void *args) {
-  sleep(100);
   int value = *(int *)args;
   if (value & 1) {
     printf("This thread will call exit with code %d\n", value);
@@ -19,23 +18,21 @@ int main(void) {
   int code = 0;
 
   clone(thread_fn, &expected_code_1);
-  // wait(&code);
+  wait(&code);
   printf("Thread exit code: %d\n", code);
   if (code != expected_code_1) {
     fprintf(2, "Expected: %d\nReturned: %d\n\n", expected_code_1, code);
     exit(1);
   }
-  sleep(10);
 
   expected_code_2 = 2;
   clone(thread_fn, &expected_code_2);
-  // wait(&code);
+  wait(&code);
   printf("Thread exit code: %d\n", code);
   if (code != expected_code_2) {
     fprintf(2, "Expected: %d\nReturned: %d\n\n", expected_code_2, code);
     exit(1);
   }
-  sleep(10);
 
   printf("TEST PASSED\n");
   exit(0);
